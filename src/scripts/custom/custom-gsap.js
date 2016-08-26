6fe8b39aace9e67047c946f3721e6c6341556336
Luke Fryer <Luke.Fryer@homeagency.co.uk>
2016-08-26 16:16:47 +0100

    more code tidying and lint fixing (js is now error and warning free)

--- src/scripts/custom/custom-gsap.js
+++ src/scripts/custom/custom-gsap.js
@@ -1,13 +1,10 @@
+// Custom script containing Tweens using the Greensock Animation Platform
 (function() {
     'use strict';
 
-    // Store Variables
-    var box = document.querySelectorAll(".box");
-        //depth = '5px'; // Must be half of CSS '$depth' variable
-
     // Flying Text Effect
-    var split = document.querySelectorAll('.words h2, .words h3, .words p, .words li');
-    var tlSplitText = new TimelineLite({
+    var split = document.querySelectorAll('.words h2, .words h3, .words p, .words li'),
+        tlSplitText = new TimelineLite({
             onCompleteAll: function() {
                 mySplitText.revert();
             }
@@ -44,7 +41,7 @@
         el.animationMaskMove = tlMove;
 
         // Mask click animation
-        var tlClick = tlLeave = new TimelineLite({
+        var tlClick = new TimelineLite({
             paused: true
         });
         tlClick.to(img, 0.8, {
@@ -52,7 +49,7 @@
         });
         tlClick.to(img, 0.4, {
             '-webkit-filter': 'grayscale(0%)',
-             filter: 'grayscale(0%)'
+            filter: 'grayscale(0%)'
         }, '-=0.8');
         el.animationMaskClick = tlClick;
         tlClick.progress(1).progress(0); // Forces an initial render of this tween so that it's cached for its 2nd usage
@@ -64,7 +61,7 @@
         tlLeave.to(img, 0.2, {
             webkitClipPath: '0 at ' + valueX + 'px ' + valueY + 'px',
             '-webkit-filter': 'grayscale(100%)',
-             filter: 'grayscale(100%)'
+            filter: 'grayscale(100%)'
         });
         el.animationMaskLeave = tlLeave;
         tlLeave.progress(1).progress(0);
@@ -77,99 +74,95 @@
             ease: Bounce.easeOut,
             webkitClipPath: '0 at ' + valueX + 'px ' + valueY + 'px',
             '-webkit-filter': 'grayscale(100%)',
-             filter: 'grayscale(100%)'
+            filter: 'grayscale(100%)'
         });
         el.animationMaskSlowLeave = tlLeaveSlow;
         tlLeaveSlow.progress(1).progress(0);
-
     }
 
-    // Force initial run with dummy coordinates (so tweens can be cached)
-    [].forEach.call(box, function(el) {
-        moveMask(el, -100, -100)
-    });
-
+    var box = document.querySelectorAll(".box");
     // Tweens that don't require access to coordinates
     [].forEach.call(box, function(el) {
+        // Force initial run with dummy coordinates (So tweens can be cached)
+        moveMask(el, -100, -100);
         // Box
-        var svgContainer = el.querySelectorAll('.svg-container');
-        var svgContainerLeft = el.querySelectorAll('.box .left');
-        var svgContainerBottom = el.querySelectorAll('.box .bottom');
+        var boxContent = el.querySelectorAll('.box-content');
+        var box3dLeft = el.querySelectorAll('.box .left');
+        var box3dRight = el.querySelectorAll('.box .bottom');
         // Reveals Box
         var img = el.querySelectorAll('.reveal-box .img');
-        var mask = el.querySelectorAll('.reveal-box .mask');
-        var text = el.querySelectorAll('.reveal-box .text');
         var elSplit = el.querySelectorAll('.reveal-box .words');
         // Skills Box
         var logo = el.querySelectorAll('.skills-box .logo');
         var svg = el.querySelectorAll('.skills-box svg');
-        var text = el.querySelectorAll('.skills-box h2');
+        var h2 = el.querySelectorAll('.skills-box h2');
         var list = el.querySelectorAll('.skills-box ul li');
 
         // Box - 3D depth animation
-        var tlContainer3d = new TimelineLite({
+        var tlBox3d = new TimelineLite({
             paused: true
         });
-        tlContainer3d.to(svgContainer, 0.3, {
+        tlBox3d.to(boxContent, 0.3, {
             x: "-=5px",
             y: "+=5px"
         });
-        el.animationContainer3d = tlContainer3d;
-        tlContainer3d.progress(1).progress(0);
+        el.animationBox3d = tlBox3d;
+        tlBox3d.progress(1).progress(0);
 
         // Box - 3D depth animation Left
-        var tlContainer3dLeft = new TimelineLite({
+        var tlBox3dLeft = new TimelineLite({
             paused: true
         });
-        tlContainer3dLeft.to(svgContainerLeft, 0.3, {
+        tlBox3dLeft.to(box3dLeft, 0.3, {
             width: "5px"
         });
-        el.animationContainer3dLeft = tlContainer3dLeft;
-        tlContainer3dLeft.progress(1).progress(0);
+        el.animationBox3dLeft = tlBox3dLeft;
+        tlBox3dLeft.progress(1).progress(0);
 
         // Box - 3D depth animation Bottom
-        var tlContainer3dBottom = new TimelineLite({
+        var tlBox3dBottom = new TimelineLite({
             paused: true
         });
-        tlContainer3dBottom.to(svgContainerBottom, 0.3, {
+        tlBox3dBottom.to(box3dRight, 0.3, {
             height: "5px",
             right: "+=5px"
         });
-        el.animationContainer3dBottom = tlContainer3dBottom;
-        tlContainer3dBottom.progress(1).progress(0);
+        el.animationBox3dBottom = tlBox3dBottom;
+        tlBox3dBottom.progress(1).progress(0);
 
         // Reveal Box - Image animation
-        var tlImgMove = new TimelineLite({
+        var tlRevealImgMove = new TimelineLite({
             paused: true
         });
-        tlImgMove.to(img, 0.3, {
+        tlRevealImgMove.to(img, 0.3, {
             scale: 1,
             ease: Sine.easeOut,
             '-webkit-filter': 'grayscale(0%)',
-             filter: 'grayscale(0%)'
+            filter: 'grayscale(0%)'
         }, 0.05);
-        el.animationImgMove = tlImgMove;
-        tlImgMove.progress(1).progress(0);
+        el.animationImgMove = tlRevealImgMove;
+        tlRevealImgMove.progress(1).progress(0);
 
         // Reveal Box - Mask click text animation
-        var tlText = new TimelineLite({
+        var tlRevealText = new TimelineLite({
             paused: true
-        })
-        tlText.to(elSplit, 0.35, {
+        });
+        tlRevealText.to(elSplit, 0.35, {
             autoAlpha: 0
         });
-        el.animationTextClick = tlText;
-        tlText.progress(1).progress(0);
+        el.animationTextClick = tlRevealText;
+        tlRevealText.progress(1).progress(0);
 
         // Skills Box - Set initial state
         TweenLite.set(logo, {
             transformOrigin: "50% 50%",
         });
+
         // Skills Box - Animate SVG path (logo)
-        var tlLogo = new TimelineLite({
+        var tlSkillsLogo = new TimelineLite({
             paused: true
-        })
-        tlLogo.to(logo, 0.6, {
+        });
+        tlSkillsLogo.to(logo, 0.6, {
             fill: "#ffffff",
             rotation: 360,
             scale: 1,
@@ -182,7 +175,7 @@
         });
 
         // Skills Box -  Animate SVG (viewbox)
-        tlLogo.to(svg, 0.6, {
+        tlSkillsLogo.to(svg, 0.6, {
             ease: Circ.easeInOut,
             attr: {
                 width: 200,
@@ -193,23 +186,23 @@
                 marginLeft: -125,
                 marginTop: -125
             },
-        }, 0)
-        tlLogo.progress(1).progress(0);
-        el.animationLogo = tlLogo;
+        }, 0);
+        tlSkillsLogo.progress(1).progress(0);
+        el.animationLogo = tlSkillsLogo;
 
         // Skills Box - Animate Text
-        var tlText = new TimelineLite({
+        var tlSkillsText = new TimelineLite({
             paused: true
         });
-        tlText.to(text, 0.3, {
+        tlSkillsText.to(h2, 0.3, {
             fontSize: '24px',
             rotation: 0,
             x: -55,
             y: -100,
             autoAlpha: 0.7,
         }, 0.3);
-        tlText.progress(1).progress(0);
-        el.animationText = tlText;
+        tlSkillsText.progress(1).progress(0);
+        el.animationText = tlSkillsText;
 
         // Skills Box - Animate list out
         /* var tlListOut = new TimelineLite({
@@ -255,12 +248,13 @@
 
     // Event listener functions
     function revealMouseMove(e) {
+        /*jshint validthis: true */
         if (revealElOver) {
 
             // Get coordinates of box
             var rect = this.getBoundingClientRect(),
-            xPos = e.pageX - rect.left,
-            yPos = e.pageY - rect.top - window.scrollY;
+                xPos = e.pageX - rect.left,
+                yPos = e.pageY - rect.top - window.scrollY;
 
             // Add coordinates to array and pass in to moveMask function
             moveMask(this, xPos, yPos);
@@ -271,28 +265,32 @@
 
     // Box
     function boxMouseDown() {
-        this.animationContainer3d.play();
-        this.animationContainer3dLeft.play();
-        this.animationContainer3dBottom.play();
+        /*jshint validthis: true */
+        this.animationBox3d.play();
+        this.animationBox3dLeft.play();
+        this.animationBox3dBottom.play();
 
-    };
+    }
 
     function boxMouseLeave() {
-        this.animationContainer3d.reverse();
-        this.animationContainer3dLeft.reverse();
-        this.animationContainer3dBottom.reverse();
+        /*jshint validthis: true */
+        this.animationBox3d.reverse();
+        this.animationBox3dLeft.reverse();
+        this.animationBox3dBottom.reverse();
 
-    };
+    }
 
     // Box - Reveal
     function revealMouseDown() {
+        /*jshint validthis: true */
         this.animationMaskClick.play();
         this.animationTextClick.play();
         revealClicked = true;
         revealElOver = false;
-    };
+    }
 
     function revealMouseLeave() {
+        /*jshint validthis: true */
         this.animationTextClick.timeScale(1.25).reverse();
         // If revealClicked then run slow animation
         if (revealClicked) {
@@ -302,24 +300,26 @@
         }
         revealClicked = false;
         revealElOver = true;
-    };
+    }
 
     // Box - Skills
     function skillsMouseDown() {
+        /*jshint validthis: true */
         this.animationLogo.play();
         this.animationText.play();
         this.animationListIn.play(0);
         skillsClicked = true;
-    };
+    }
 
     function skillsMouseLeave() {
+        /*jshint validthis: true */
         if (skillsClicked) {
             this.animationLogo.reverse(0);
             this.animationText.reverse(1);
             this.animationListIn.progress(0).pause();
         }
         skillsClicked = false;
-    };
+    }
 
 
 }());
